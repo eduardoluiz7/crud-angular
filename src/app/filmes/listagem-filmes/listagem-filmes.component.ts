dca1fd257c276670b36f4a00ebce1775d6d69d4c
Eduardo Luiz <els6@cin.ufpe.br>
2021-05-20 21:30:57 -0300

    projeto finalizado

---
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
+import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigParams } from 'src/app/shared/models/config-params';
+import { Filme } from 'src/app/shared/models/filme';
+
+@Component({
+  selector: 'dio-listagem-filmes',
+  templateUrl: './listagem-filmes.component.html',
+  styleUrls: ['./listagem-filmes.component.scss']
+})
+export class ListagemFilmesComponent implements OnInit {
+
+  readonly semFoto = 'https://www2.camara.leg.br/atividade-legislativa/comissoes/comissoes-permanentes/cindra/imagens/sem.jpg.gif/image';
+  config: ConfigParams = {
+    pagina: 0,
+    limite: 8
+  }
+  filmes: Array<Filme> = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+
+  constructor(private filmeService: FilmesService, private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges
+    .pipe(debounceTime(400))
+    .subscribe((val: string)=>{
+      this.config.pesquisa = val;
+      this.resetarConsulta();
+    });
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string)=>{
+      this.config.campo = {tipo: 'genero' , valor: val};
+      this.resetarConsulta()
+    })
+
+    this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção Científica', 'Comédia', 'Drama'];
+
+    this.listarFilmes();
+  }
+
+  onScroll(): void{
+    this.listarFilmes();
+  }
+
+  abrir(id: number): void{
+    this.router.navigateByUrl('/filmes/' + id);
+  }
+
+  private listarFilmes(): void {
+    this.config.pagina++;
+    this.filmeService.listar(this.config).subscribe({
+      next: (filmes: Filme[])=>{
+        this.filmes.push(...filmes);
+      }
+    });
+  }
+
+  private resetarConsulta(): void{
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
+  }
+}
